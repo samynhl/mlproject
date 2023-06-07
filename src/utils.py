42faceeaa34b68122ba4b8930e01edf8d2f8bec8
unknown <im_nehlil@esi.dz>
2023-06-07 09:33:46 +0200

    Hp tuning

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 from src.logger import logging
@@ -23,7 +24,7 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, param):
     """Evaluates the performance of the models.
     """
     try :
@@ -31,7 +32,13 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
     
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
             #Train the model
+            model.set_params(gs.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
